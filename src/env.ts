dda02bc339f3f9058ed34017ba31bac3249060c4
NowisFurther <35854533+nowisf@users.noreply.github.com>
2023-02-21 21:33:13 -0300

    Revert "12 auth forgot password"

--- src/env.ts
+++ src/env.ts
@@ -9,5 +9,3 @@ dotenv.config({
 
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const JWT_SECRET = process.env.JWT_SECRET || "";
-//TODO: XD
-export const ROUNDS = 10;
