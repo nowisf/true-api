26030d3e3742274d160518014a9c82c7d53dfd79
Simon Busques <simonbusques@gmail.com>
2023-02-24 14:32:49 -0300

    feat: decorator authentificated implemented; util decodeToken implemented

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,18 @@
+import { server } from ".";
+
 export const spacesRegex = new RegExp(/\s+/g);
+
+interface TokenContent {
+  id: string;
+  email: string;
+}
+
+export const decodeToken = (token: string) => {
+  const decoded = server.jwt.decode(token);
+
+  if (!decoded) {
+    throw new Error("Token could not be decoded, please try again");
+  }
+
+  return decoded as TokenContent;
+};
