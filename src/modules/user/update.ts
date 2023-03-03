1b5b74ceb5009c926d54166eb5d95d45a39739d6
Simon Busques <simonbusques@gmail.com>
2023-03-02 22:02:23 -0300

    feat: add update user endpoint to admins
    # Conflicts:
    #	src/modules/user/types.ts
    #	src/modules/user/update.ts

--- src/modules/user/update.ts
+++ src/modules/user/update.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../../database";
+<<<<<<< HEAD
 import { RequestParams, UserUpdateProps } from "./types";
 
 export async function update(req: FastifyRequest, reply: FastifyReply) {
@@ -16,4 +17,25 @@ export async function update(req: FastifyRequest, reply: FastifyReply) {
     return reply.code(404).send("User not found");
   }
   return reply.send({ user });
+=======
+import { UserUpdateProps } from "./types";
+
+interface RequestParams {
+  id: string;
+}
+
+export async function update(req: FastifyRequest, reply: FastifyReply) {
+  const { id } = req.params as RequestParams;
+  const user = prisma.user.findUnique({ where: { id } });
+  if (!user) {
+    return reply.code(404).send("User not found");
+  }
+
+  const { password, role, active } = req.body as UserUpdateProps;
+
+  await prisma.user.update({
+    where: { id },
+    data: { password, role, active },
+  });
+>>>>>>> 5dd2e1e (feat: add update user endpoint to admins)
 }
