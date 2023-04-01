2c24ab8e10d51118870d63f26d3dde76b9a8fcae
Simon Busques <simonbusques@gmail.com>
2023-03-31 23:32:37 -0300

    fix: remove health

--- src/fastify.ts
+++ src/fastify.ts
@@ -2,13 +2,11 @@ import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { JWT_SECRET } from "./env";
 import { auth } from "./modules/auth";
-import { health } from "./modules/health";
 import { user } from "./modules/user";
 
 export const server = fastify();
 
 // routes
-server.register(health);
 server.register(auth, { prefix: "/api/auth" });
 server.register(user, { prefix: "/api/user" });
 
