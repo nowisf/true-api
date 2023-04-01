051b2c7bce06f718525755065bc85e25246fbfbb
NowisFurther <35854533+nowisf@users.noreply.github.com>
2023-03-31 23:55:57 -0300

    fix: railwind (#81)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { PORT } from "./env";
 import { server } from "./fastify";
+// @ts-expect-error no types
+import vault from "dotenv-vault-core";
+vault.config();
 
 server.listen({ port: PORT }, (err, address) => {
   if (err) {
