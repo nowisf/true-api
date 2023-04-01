321dd323d8bb1436ad4df71de68dd6d3ef2eca6b
Juan Latorre <juanlatorreharcha@gmail.com>
2023-04-01 00:24:22 -0300

    chore: add host

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { PORT } from "./env";
 import { server } from "./fastify";
 
-server.listen({ port: PORT }, (err, address) => {
+server.listen({ host: "0.0.0.0", port: PORT }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
